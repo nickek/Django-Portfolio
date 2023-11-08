b9e79b785d667042ae7436e8c1d5d54f17b8bd49
nickek <nickekthou@gmail.com>
2023-11-08 15:04:50 -0500

    First and Last name add

--- home/forms.py
+++ home/forms.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 
 
 class CreateUserForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=25)
+    last_name = forms.CharField(max_length=25)
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
